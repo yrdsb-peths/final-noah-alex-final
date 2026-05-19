2e7e8e5f102a64fadfd0c710f3f9013950841a7a
alexzhao3487-boop <alexzhao3487@gmail.com>
2026-05-19 11:49:54 -0400

    created titlescreen with hero placeholder

---
+++ TitleScreen.java
@@ -0,0 +1,57 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TitleScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TitleScreen extends World
+{
+    Label titleLabel = new Label("Hungry Elephant", 60);
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+
+        addObject(titleLabel, getWidth()/2, 200);
+        prepare();
+    }
+
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("space"))
+        {
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+        }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Hero elephant = new Hero();
+        addObject(elephant,494,83);
+        elephant.setLocation(508,162);
+        elephant.setLocation(488,93);
+        elephant.setLocation(513,141);
+        Label label = new Label("press space to start", 50);
+        addObject(label,128,198);
+        label.setLocation(334,198);
+        label.setLocation(292,222);
+        Label label2 = new Label("use \u2190 and \u2192 arrows to move", 30);
+        addObject(label2,106,292);
+        label2.setLocation(389,300);
+        label.setLocation(324,249);
+        label.setLocation(328,210);
+        label.setLocation(346,98);
+        label.setLocation(247,359);
+    }
+}
