e7fd1bfe8b392200ef3525451cbaa2c100ba7435
lumbers <noahlum2009@gmail.com>
2026-05-20 12:07:36 -0400

    Update Hero.java

--- Hero.java
+++ Hero.java
@@ -1,88 +1,116 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Hero here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Hero extends Actor
 {
-    /**
-     * Act - do whatever the Hero wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    
     private int laserCooldown = 0;
     private int hp = 5;
-    private int invincibilityTimer = 0; // Cooldown after getting hit
+    private int invincibilityTimer = 0; 
     private HpBar healthBar;
+    
+    // --- Friend's Sprite Variables ---
+    private GreenfootImage idleImage;
+    private GreenfootImage upImage;
+    private GreenfootImage leftImage;
+    private GreenfootImage rightImage;
+
+    // --- Constructor (Loads and scales images once at the start) ---
+    public Hero() 
+    {
+        idleImage = new GreenfootImage("baseguy.png");
+        idleImage.scale(50, 50);
+        
+        upImage = new GreenfootImage("baseguy-up.png");
+        upImage.scale(50, 50);
+        
+        leftImage = new GreenfootImage("baseguy-left.png");
+        leftImage.scale(50, 50);
+        
+        rightImage = new GreenfootImage("baseguy-right.png");
+        rightImage.scale(50, 50);
+        
+        // Set the initial appearance
+        setImage(idleImage);
+    }
     
     public void setHpBar(HpBar bar)
     {
         this.healthBar = bar;
     }
+
     public void act()
     {
+        // 1. Friend's Mouse Tracking (with rotation correction)
         MouseInfo mouse = Greenfoot.getMouseInfo();
-    
-    if (mouse != null) 
-    {
-        turnTowards(mouse.getX(), mouse.getY());
-    }
+        if (mouse != null) 
+        {
+            turnTowards(mouse.getX(), mouse.getY());
+            setRotation(getRotation() + 90);
+        }
         
-        // Add your action code here.
-        GreenfootImage image = new GreenfootImage("baseguy.png");
-        image.scale(50, 50);
-        setImage(image);
+        // Track if a key is held down to manage idle states
+        boolean keyIsPressed = false;
+
+        // 2. Movement & Sprite Swapping (Combined)
         if (Greenfoot.isKeyDown("a"))
         {
             setLocation(getX() - 5, getY());
+            setImage(leftImage);
+            keyIsPressed = true;
         }
         if (Greenfoot.isKeyDown("d"))
         {
             setLocation(getX() + 5, getY());
+            setImage(rightImage);
+            keyIsPressed = true;
         }
         if (Greenfoot.isKeyDown("w"))
         {
             setLocation(getX(), getY() - 5);
+            setImage(upImage);
+            keyIsPressed = true;
         }
         if (Greenfoot.isKeyDown("s"))
         {
             setLocation(getX(), getY() + 5);
+            setImage(idleImage); 
+            keyIsPressed = true;
         }
         
-        //laser stuff
+        // If no keys are pressed, return to base idle sprite
+        if (!keyIsPressed) 
+        {
+            setImage(idleImage);
+        }
+        
+        // 3. Laser Cooldown Ticker
         if (laserCooldown > 0) {
-            laserCooldown--; // Decrease cooldown every frame
+            laserCooldown--; 
         }
         
-        // Shoot when space is pressed and cooldown is ready
-        if (Greenfoot.isKeyDown("space") && laserCooldown == 0)
+        // 4. Friend's Mouse Click Shooting (with matching laser rotation fix)
+        if (Greenfoot.mousePressed(null) && laserCooldown == 0)
         {
             Lazer laser = new Lazer();
             getWorld().addObject(laser, getX(), getY());
-            
-            // Match the laser's rotation to the alligator's mouth orientation
-            laser.setRotation(getRotation()); 
-            
-            laserCooldown = 20; // Wait about 1/3 of a second before shooting again
+            laser.setRotation(getRotation() - 90); 
+            laserCooldown = 20; 
         }
         
+        // 5. Your Invincibility Frame Polish
         if (invincibilityTimer > 0)
         {
             invincibilityTimer--;
-            
-            // Optional Polish: Make the alligator blink or turn translucent while invincible
             if (invincibilityTimer % 4 == 0) {
-                getImage().setTransparency(100); // Semi-transparent
+                getImage().setTransparency(100); 
             } else {
-                getImage().setTransparency(255); // Normal
+                getImage().setTransparency(255); 
             }
         }
         else
         {
-            getImage().setTransparency(255); // Reset transparency completely
+            if (getImage() != null) {
+                getImage().setTransparency(255); 
+            }
         }
         
         checkEnemyContact();
@@ -90,15 +118,11 @@ public class Hero extends Actor
     
     private void checkEnemyContact()
     {
-        // If touching a fish AND not currently invincible
         if (isTouching(Fish.class) && invincibilityTimer == 0)
         {
-            hp--; // Take 1 damage instantly
+            hp--; 
+            invincibilityTimer = 30;  
             
-            // Give the player 30 frames (0.5 seconds) of invincibility to escape
-            invincibilityTimer = 30; 
-            
-            // Update the health bar UI
             if (healthBar != null)
             {
                 healthBar.updateBar(hp);
