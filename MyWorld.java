9bc3d4f4b1b2c05693d6164022a2f0abcc12d87f
alexzhao3487-boop <alexzhao3487@gmail.com>
2026-05-21 11:19:22 -0400

    fish now spawn from the edges

--- MyWorld.java
+++ MyWorld.java
@@ -28,13 +28,32 @@ public class MyWorld extends World {
     }
     
     private void spawnFish()
+{
+    Fish enemy = new Fish();
+    
+    int x, y;
+    int edge = Greenfoot.getRandomNumber(4); // 0=top, 1=bottom, 2=left, 3=right
+    
+    switch (edge)
     {
-        Fish enemy = new Fish();
-        
-        // Pick a random X between 0 and 599, and random Y between 0 and 399
-        int randomX = Greenfoot.getRandomNumber(getWidth());
-        int randomY = Greenfoot.getRandomNumber(getHeight());
-        
-        addObject(enemy, randomX, randomY);
+        case 0: // Top edge
+            x = Greenfoot.getRandomNumber(getWidth());
+            y = 0;
+            break;
+        case 1: // Bottom edge
+            x = Greenfoot.getRandomNumber(getWidth());
+            y = getHeight() - 1;
+            break;
+        case 2: // Left edge
+            x = 0;
+            y = Greenfoot.getRandomNumber(getHeight());
+            break;
+        default: // Right edge
+            x = getWidth() - 1;
+            y = Greenfoot.getRandomNumber(getHeight());
+            break;
     }
+    
+    addObject(enemy, x, y);
+}
 }
