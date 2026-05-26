773e7a961338da5d82f7259f0dbdc8e7cbe68ad6
lumbers <noahlum2009@gmail.com>
2026-05-26 11:43:35 -0400

    Fixed HP bar so it works for everything

--- HpBar.java
+++ HpBar.java
@@ -12,38 +12,54 @@ public class HpBar extends Actor
      * Act - do whatever the HpBar wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    private final int MAX_HP = 10;
-    private final int BAR_WIDTH = 150;
-    private final int BAR_HEIGHT = 20;
+    private int maxHp = 10;
+    private int currentHp = 10;
+    private int barWidth = 150;
+    private int barHeight = 20;
+    private Color barColor = Color.RED;
 
     public HpBar()
     {
-        updateBar(MAX_HP);
+        updateBar(maxHp);
     }
     
-    public void updateBar(int currentHp)
+    public void setMaxHp(int newMax)
     {
+        this.maxHp = newMax;
+        this.currentHp = newMax;
+        updateBar(currentHp);
+    }
+    
+    public void setLineColor(Color newColor)
+    {
+        this.barColor = newColor;
+        updateBar(currentHp);
+    }
+    
+    public void updateBar(int hp)
+    {
+        this.currentHp = hp;
+        
         // 1. Create a blank image to act as our canvas
-        GreenfootImage image = new GreenfootImage(BAR_WIDTH, BAR_HEIGHT);
+        GreenfootImage image = new GreenfootImage(barWidth, barHeight);
         
         // 2. Draw the white background/outline frame
         image.setColor(Color.WHITE);
-        image.drawRect(0, 0, BAR_WIDTH - 1, BAR_HEIGHT - 1);
+        image.drawRect(0, 0, barWidth - 1, barHeight - 1);
         
         // 3. Prevent errors if HP goes below 0
         if (currentHp < 0) {
             currentHp = 0;
         }
         
-        // 4. Calculate how wide the red rectangle should be
-        // (Current HP / Max HP) * Total Width
-        int redWidth = (int)(((double)currentHp / MAX_HP) * (BAR_WIDTH - 4));
+        // 4. Calculate how wide the inner rectangle should be
+        int fillWidth = (int)(((double)currentHp / maxHp) * (barWidth - 4));
         
-        // 5. Fill in the red health meter if we have health left
-        if (redWidth > 0)
+        // 5. Fill in the meter using our dynamic barColor variable
+        if (fillWidth > 0)
         {
-            image.setColor(Color.RED);
-            image.fillRect(2, 2, redWidth, BAR_HEIGHT - 4);
+            image.setColor(barColor);
+            image.fillRect(2, 2, fillWidth, barHeight - 4);
         }
         
         // Set the newly drawn image to this actor
