ae6dc5e77c8099fe05c393f31047899e3e8fdca4
lumbers <noahlum2009@gmail.com>
2026-05-28 11:48:18 -0400

    Update Trident.java

--- Trident.java
+++ Trident.java
@@ -17,28 +17,28 @@ public class Trident extends Actor
     public void setCarried(boolean c) { carried = c; }
     
     public void act()
-{
-    if (carried)
     {
-        // Follow hero position with a small offset
-        List<Hero> heroes = getWorld().getObjects(Hero.class);
-        if (!heroes.isEmpty())
+        if (carried)
         {
-            Hero hero = heroes.get(0);
-            setLocation(hero.getX() + 15, hero.getY() + 15);
+            // Follow hero position with a small offset
+            List<Hero> heroes = getWorld().getObjects(Hero.class);
+            if (!heroes.isEmpty())
+            {
+                Hero hero = heroes.get(0);
+                setLocation(hero.getX() + 15, hero.getY() + 15);
+            }
+        }
+        else if (flying)
+        {
+            move(15);
+            pierceEnemies();
+            if (isAtEdge())
+            {
+                flying = false;
+                stuck = true;
+            }
         }
     }
-    else if (flying)
-    {
-        move(15);
-        pierceEnemies();
-        if (isAtEdge())
-        {
-            flying = false;
-            stuck = true;
-        }
-    }
-}
 
     private void pierceEnemies()
     {
@@ -53,24 +53,24 @@ public class Trident extends Actor
         List<Pufferfish> puffers = getObjectsInRange(15, Pufferfish.class);
         for (Pufferfish p : puffers)
         {
-            p.takeDamage(5); // you'll need to add takeDamage to Pufferfish (see below)
+            p.takeDamage(5);
         }
 
         // Damage Boss
         List<SwordfishBoss> bosses = getObjectsInRange(15, SwordfishBoss.class);
         for (SwordfishBoss b : bosses)
         {
-            b.takeDamage(5); // you'll need to add takeDamage to SwordfishBoss (see below)
+            b.takeDamage(5); 
         }
     }
 
     public void launch(int angle)
-{
-    carried = false;
-    setRotation(angle);
-    flying = true;
-    stuck = false;
-}
+    {
+        carried = false;
+        setRotation(angle);
+        flying = true;
+        stuck = false;
+    }
 
     public boolean isStuck() { return stuck; }
     public boolean isFlying() { return flying; }
